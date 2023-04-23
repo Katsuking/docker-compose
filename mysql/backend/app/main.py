8cfe08adae37e90c8ddcc38bda4b7484e68213fb
Katsuking <legendsoccerforte5@gmail.com>
2023-04-23 15:25:20 +0900

    環境構築時

--- mysql/backend/app/main.py
+++ mysql/backend/app/main.py
@@ -1,7 +1,24 @@
+import mysql.connector
+from typing import List
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello world"}
+
+@app.get("/users")
+def get_users() -> List[dict]:
+    conn = mysql.connector.connect(
+        user="myuser",
+        password="mypassword",
+        host="db",
+        database="mydatabase",
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return rows
