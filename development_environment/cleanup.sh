ecbadcff28c54a365220c12794633f1cf008078c
Katsuking <legendsoccerforte5@gmail.com>
2023-09-09 13:06:09 +0900

    Manual update!

---
+++ development_environment/cleanup.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+
+docker compose down
+
+image_name="v1_dev"
+container=$(docker ps -a | grep ${image_name} | awk '{print $2}')
+img=$(docker images | grep ${image_name} | awk '{print $3}')
+
+if [[ -n ${container} ]]; then
+    container_id=$(docker ps -a | grep ${image_name} | awk '{print $1}')
+    # docker container stop ${container_id}
+    docker container rm "${container_id}"
+fi
+
+if [[ -n ${img} ]]; then
+    docker image rm "${img}"
+fi
+
+read -p "Do you want to delete volume attached to ${image_name}? (y/n) " answer
+if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+    docker volume rm "$(basename "$(pwd)")_${image_name}" 2>/dev/null
+fi
+
